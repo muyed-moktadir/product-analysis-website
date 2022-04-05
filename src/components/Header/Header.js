b7d9702c01e8760743de935dd917ba478c7640a8
muyed moktadir <muyed004@gmail.com>
2022-04-05 09:28:54 +0600

    create components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>WelCome To Cozy Library</h1>
+            <nav>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
