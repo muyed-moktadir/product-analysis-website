67f825d9c69a14ac1f5ab97d669ddd0a8a59d397
muyed moktadir <muyed004@gmail.com>
2022-04-05 10:07:33 +0600

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1>WelCome To Cozy Library</h1>
-            <nav>
+            <nav >
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/reviews">Reviews</CustomLink>
+                <CustomLink to="/dashboard">Dashboard</CustomLink>
+                <CustomLink to="blogs">Blogs</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
             </nav>
         </div>
     );
