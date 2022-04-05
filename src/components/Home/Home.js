61d03d2776e43555ae3a95b9056b3dd27374d6d7
muyed moktadir <muyed004@gmail.com>
2022-04-05 11:15:47 +0600

    add image and info

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
+import React from "react";
+import b_img from '../image/b_img.jpg';
+import './Home.css'
 const Home = () => {
-    return (
+  return (
+    <div>
+      <div className="cozy-library-info">
         <div>
-          <h1>home</h1>  
+          <h1>Cartoon Book's World !!</h1>
+          <p>
+            “Yesterday is history, tomorrow is a mystery, and today is a gift...
+            that's why they call it the present” - Master Oogway, 'Kung Fu
+            Panda’.
+          </p>
         </div>
-    );
+        <div>
+            <img src={b_img} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;<h1>home</h1>
+export default Home;
