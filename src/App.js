eaae45137032448cf6c7f0ac91bcf701d658e98b
muyed moktadir <muyed004@gmail.com>
2022-04-05 10:19:16 +0600

    adding route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
+import Reviews from './components/Reviews/Reviews';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
         <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+          <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+        </Routes>
     </div>
   );
 }
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const About = () => {
+const Blogs = () => {
     return (
         <div>
-            <h1>about</h1>
+            <h1>blogs</h1>
         </div>
     );
 };
 
-export default About;
+export default Blogs;
